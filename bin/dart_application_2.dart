be547678307b0dd3999c2381dcc76de8be90abbc
Marcos Piemontez <marcos.piemontez1@gmail.com>
2024-01-09 18:17:06 -0400

    feat: study method set

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,5 +1,55 @@
-import 'package:dart_application_2/dart_application_2.dart' as dart_application_2;
+void main() {
+  escolherMeioTransporte(Transporte.carro);
+  Set<String> registrosVisitados = <String>{};
+  registrosVisitados = registrarDestinos("Rio Grande do Sul", registrosVisitados);
+  registrosVisitados = registrarDestinos("Santa Catarina", registrosVisitados);
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_2.calculate()}!');
+  Map<String, dynamic> registrarPrecos = {};
+
+  registrarPrecos["São Paulo"] = 1200.0;
+  registrarPrecos["Rio de Janeiro"] = 1500.0;
+  registrarPrecos.remove("São Paulo");
+
+  print(registrarPrecos);
+}
+
+Set<String> registrarDestinos(String destino, Set<String> banco) {
+  banco.add(destino);
+  return banco;
+}
+
+void escolherMeioTransporte(Transporte locomocao) {
+  String mensagem;
+
+  switch (locomocao) {
+    case Transporte.carro:
+    case Transporte.bike:
+    case Transporte.andando:
+    case Transporte.skate:
+    case Transporte.aviao:
+    case Transporte.onibus:
+    case Transporte.trem:
+      mensagem = "Vou de $locomocao para Aventura";
+      break;
+    default:
+      mensagem = "Meio de transporte não reconhecido";
+  }
+
+  print(mensagem);
+}
+
+enum Transporte {
+  carro,
+  bike,
+  andando,
+  skate,
+  aviao,
+  onibus,
+  trem,
+}
+
+class Viagem {
+  double dinheiro;
+
+  Viagem(this.dinheiro);
 }
